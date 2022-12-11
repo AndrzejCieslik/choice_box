374ac1d687158352c7dbbb954d2cf5f4488e9b34
Andrzej <acieslik.enn@gmail.com>
2022-12-11 15:05:15 +0100

    Initial commit

---
+++ src/main/java/com/example/choice_box/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.choice_box;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
